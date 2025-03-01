e41a1ff2dd4b539326ac20f4ed6a6f9600c367d7
k-natori1025 <k-natori1025@gmail.com>
2025-03-01 20:39:02 +0900

    Thymleafを使ってレスポンスを生成

--- src/main/java/com/example/spring_todo_app/controller/IndexController.java
+++ src/main/java/com/example/spring_todo_app/controller/IndexController.java
@@ -2,16 +2,14 @@ package com.example.spring_todo_app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
     // localhost:8080 -> "Hello, world!"
     @GetMapping("/")
-    @ResponseBody
     public String index() {
-        return "Hello, world!";
+        return "index";
     }
 
 }
