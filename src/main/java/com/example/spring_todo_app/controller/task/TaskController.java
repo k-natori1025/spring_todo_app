d78bf66d433ac45427970fb5a25d93cdb929818e
k-natori1025 <k-natori1025@gmail.com>
2025-03-02 21:11:50 +0900

    Lombokを使ってボイラーコードを削除

--- src/main/java/com/example/spring_todo_app/controller/task/TaskController.java
+++ src/main/java/com/example/spring_todo_app/controller/task/TaskController.java
@@ -1,20 +1,18 @@
 package com.example.spring_todo_app.controller.task;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.spring_todo_app.service.task.TaskService;
 
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class TaskController {
 
     private final TaskService taskService;
-
-    public TaskController(TaskService taskService) {
-        this.taskService = taskService;
-    }
 
     @GetMapping("/tasks")
     public String list(Model model) {
